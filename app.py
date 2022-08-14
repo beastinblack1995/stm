c4ed3547affe948e0ef83915f06a8add76f21387
beastinblack1995 <109196514+beastinblack1995@users.noreply.github.com>
2022-08-14 15:53:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -134,21 +134,31 @@ RTC_CONFIGURATION = RTCConfiguration(
 
 
 def main():
-    st.header("WebRTC demo")
-
+    st.header("Attendence_system")
+    
+#     pages = {
+#         "Real time object detection (sendrecv)": app_object_detection,
+#         "Real time video transform with simple OpenCV filters (sendrecv)": app_video_filters,  # noqa: E501
+#         "Real time audio filter (sendrecv)": app_audio_filter,
+#         "Delayed echo (sendrecv)": app_delayed_echo,
+#         "Consuming media files on server-side and streaming it to browser (recvonly)": app_streaming,  # noqa: E501
+#         "WebRTC is sendonly and images are shown via st.image() (sendonly)": app_sendonly_video,  # noqa: E501
+#         "WebRTC is sendonly and audio frames are visualized with matplotlib (sendonly)": app_sendonly_audio,  # noqa: E501
+#         "Simple video and audio loopback (sendrecv)": app_loopback,
+#         "Configure media constraints and HTML element styles with loopback (sendrecv)": app_media_constraints,  # noqa: E501
+#         "Control the playing state programatically": app_programatically_play,
+#         "Customize UI texts": app_customize_ui_texts,
+#     }
+    
     pages = {
-        "Real time object detection (sendrecv)": app_object_detection,
-        "Real time video transform with simple OpenCV filters (sendrecv)": app_video_filters,  # noqa: E501
-        "Real time audio filter (sendrecv)": app_audio_filter,
-        "Delayed echo (sendrecv)": app_delayed_echo,
-        "Consuming media files on server-side and streaming it to browser (recvonly)": app_streaming,  # noqa: E501
-        "WebRTC is sendonly and images are shown via st.image() (sendonly)": app_sendonly_video,  # noqa: E501
-        "WebRTC is sendonly and audio frames are visualized with matplotlib (sendonly)": app_sendonly_audio,  # noqa: E501
-        "Simple video and audio loopback (sendrecv)": app_loopback,
-        "Configure media constraints and HTML element styles with loopback (sendrecv)": app_media_constraints,  # noqa: E501
-        "Control the playing state programatically": app_programatically_play,
-        "Customize UI texts": app_customize_ui_texts,
-    }
+
+        "Recognise employee": app_video_filters,  # noqa: E501
+
+    }    
+    
+    
+    
+    
     page_titles = pages.keys()
 
     page_title = st.sidebar.selectbox(
@@ -181,7 +191,7 @@ def app_loopback():
 def app_video_filters():
     """Video transforms with OpenCV"""
 
-    _type = st.radio("Select transform type", ("noop", "cartoon", "edges", "rotate"))
+#    _type = st.radio("Select transform type", ("noop", "cartoon", "edges", "rotate"))
 
     def callback(frame: av.VideoFrame) -> av.VideoFrame:
         img = frame.to_ndarray(format="bgr24")
@@ -217,37 +227,37 @@ def app_video_filters():
         
         
 
-        if _type == "noop":
-            pass
-        elif _type == "cartoon":
-            # prepare color
-            img_color = cv2.pyrDown(cv2.pyrDown(img))
-            for _ in range(6):
-                img_color = cv2.bilateralFilter(img_color, 9, 9, 7)
-            img_color = cv2.pyrUp(cv2.pyrUp(img_color))
+#         if _type == "noop":
+#             pass
+#         elif _type == "cartoon":
+#             # prepare color
+#             img_color = cv2.pyrDown(cv2.pyrDown(img))
+#             for _ in range(6):
+#                 img_color = cv2.bilateralFilter(img_color, 9, 9, 7)
+#             img_color = cv2.pyrUp(cv2.pyrUp(img_color))
 
-            # prepare edges
-            img_edges = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-            img_edges = cv2.adaptiveThreshold(
-                cv2.medianBlur(img_edges, 7),
-                255,
-                cv2.ADAPTIVE_THRESH_MEAN_C,
-                cv2.THRESH_BINARY,
-                9,
-                2,
-            )
-            img_edges = cv2.cvtColor(img_edges, cv2.COLOR_GRAY2RGB)
+#             # prepare edges
+#             img_edges = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+#             img_edges = cv2.adaptiveThreshold(
+#                 cv2.medianBlur(img_edges, 7),
+#                 255,
+#                 cv2.ADAPTIVE_THRESH_MEAN_C,
+#                 cv2.THRESH_BINARY,
+#                 9,
+#                 2,
+#             )
+#             img_edges = cv2.cvtColor(img_edges, cv2.COLOR_GRAY2RGB)
 
-            # combine color and edges
-            img = cv2.bitwise_and(img_color, img_edges)
-        elif _type == "edges":
-            # perform edge detection
-            img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)
-        elif _type == "rotate":
-            # rotate image
-            rows, cols, _ = img.shape
-            M = cv2.getRotationMatrix2D((cols / 2, rows / 2), frame.time * 45, 1)
-            img = cv2.warpAffine(img, M, (cols, rows))
+#             # combine color and edges
+#             img = cv2.bitwise_and(img_color, img_edges)
+#         elif _type == "edges":
+#             # perform edge detection
+#             img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)
+#         elif _type == "rotate":
+#             # rotate image
+#             rows, cols, _ = img.shape
+#             M = cv2.getRotationMatrix2D((cols / 2, rows / 2), frame.time * 45, 1)
+#             img = cv2.warpAffine(img, M, (cols, rows))
 
         return av.VideoFrame.from_ndarray(img, format="bgr24")
 
