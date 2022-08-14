70a1df97526e3f8b2a6271a89dda68d1fbcb62f7
beastinblack1995 <109196514+beastinblack1995@users.noreply.github.com>
2022-08-14 15:44:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -206,7 +206,7 @@ def app_video_filters():
             y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
 
             cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 3)
-            cv2.rectangle(img, (x1, y2-25), (x2, y2), (0, 255, 0), cv2.FILLED)
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), cv2.FILLED)
             cv2.putText(img, name, (x1+6, y2-6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
 
             startX, startY, endX, endY = faceloc
