012d54a63abc8011fc3f39540d4910f3f908c1c4
beastinblack1995 <109196514+beastinblack1995@users.noreply.github.com>
2022-08-16 12:45:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -193,11 +193,11 @@ def takepic():
                                  
     img_file_buffer = st.camera_input("Mark Your Attendence")
     
-     if st.button('show attendence'):
-         st.dataframe(pd.read_csv('attendence.csv'))
-     if st.button('clear attendence'):
-         dx = pd.DataFrame() 
-         dx.to_csv('attendence.csv')         
+    if st.button('show attendence'):
+        st.dataframe(pd.read_csv('attendence.csv'))
+    if st.button('clear attendence'):
+        dx = pd.DataFrame() 
+        dx.to_csv('attendence.csv')         
                                  
 
     if img_file_buffer is not None:
