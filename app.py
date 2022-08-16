5356fe9fb807ccd0f6983187d14dc66d8a008378
beastinblack1995 <109196514+beastinblack1995@users.noreply.github.com>
2022-08-16 12:36:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -189,14 +189,16 @@ def emprec(img):
     
     
 def takepic():
-#     if st.button('show attendence'):
-#         st.dataframe(pd.read_csv('attendence.csv'))
-#     if st.button('clear attendence'):
-#         dx = pd.DataFrame() 
-#         dx.to_csv('attendence.csv')         
-                             
+
                                  
     img_file_buffer = st.camera_input("Mark Your Attendence")
+    
+     if st.button('show attendence'):
+         st.dataframe(pd.read_csv('attendence.csv'))
+     if st.button('clear attendence'):
+         dx = pd.DataFrame() 
+         dx.to_csv('attendence.csv')         
+                                 
 
     if img_file_buffer is not None:
         # To read image file buffer as a PIL Image:
